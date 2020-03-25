9f241c14b7f5acd79e1e0030a3992a4b6637639e
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-03-25 16:03:50 +0100

    Previews work, note it would better to dynamically generate pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
-import React from "react"
+import React, { useMemo } from "react"
 import { graphql } from 'gatsby';
+import { mergePrismicPreviewData } from 'gatsby-source-prismic'
+
+// Returns true if we're in a browser, false otherwise. This will help guard
+// against SSR issues when building the site.
+const IS_BROWSER = typeof window !== 'undefined'
 
 export const query = graphql`{
     allPrismicHomepage {
@@ -19,7 +24,22 @@ export const query = graphql`{
     }
 }`;
 
-export default ({ data, ...props}) => {
-    const { node } = data.allPrismicHomepage.edges.pop();
-    return (<div>{node.data.title.text} From prismic</div>)
+export default ({ data: staticData, ...props}) => {
+    const data = useMemo(() => {
+        // If we're not in a browser (i.e. we're in SSR) or preview data has not been
+        // set, use the non-preview static data.
+        console.log("useMemo")
+        console.log(window.__PRISMIC_PREVIEW_DATA__);
+        console.log(staticData);
+        if (!IS_BROWSER || !window.__PRISMIC_PREVIEW_DATA__) return staticData.allPrismicHomepage.edges[0].node.data
+        console.log("We have a preview")
+    
+        return mergePrismicPreviewData({
+          staticData: staticData.allPrismicHomepage.edges[0].node.data,
+          previewData: window.__PRISMIC_PREVIEW_DATA__.prismicHomepage.data
+        })
+    }, [staticData])
+    console.log("index.jsx");
+    console.log({ data });
+        return (<div>{data.title.text} From prismic</div>)
 }
---
+++ src/pages/preview.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
+import { usePrismicPreview } from 'gatsby-source-prismic'
+
+const REPO_NAME = "gatsby-hello";
+const PreviewPage = () => {
+    const { isPreview, previewData, path } = usePrismicPreview({
+        // The repositoryName value from your `gatsby-config.js`.
+        repositoryName: REPO_NAME,
+    });
+
+    // This useEffect runs when values from usePrismicPreview update. When
+    // preview data is available, this will save the data globally and redirect to
+    // the previewed document's page.
+    useEffect(() => {
+        // If this is not a preview, skip.
+        //   null = Not yet determined if previewing.
+        //   true = Preview is available.
+        //   false = Preview is not available.
+        if (isPreview === false) return
+
+        // Save the preview data to somewhere globally accessible. This could be
+        // something like a global Redux store or React context.
+        //
+        // We'll just put it on window.
+        window.__PRISMIC_PREVIEW_DATA__ = previewData
+        console.log("preview pages")
+        console.log({ previewData });
+
+        if (previewData) navigate(path);
+    }, [isPreview, previewData, path]);
+
+    // Tell the user if this is not a preview.
+    if (isPreview === false) return <div>Not a preview!</div>
+
+    return <div>Loading preview...</div>
+}
+
+export default PreviewPage
