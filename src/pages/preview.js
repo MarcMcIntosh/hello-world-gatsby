b8e5e1c8cd9dfe6bd4e53a1d4e037362b1898712
Vincent Velociter <vincent.velociter@gmail.com>
2020-05-13 16:08:34 +0200

    Tweaks

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -30,7 +30,7 @@ const PreviewPage = () => {
         //   null = Not yet determined if previewing.
         //   true = Preview is available.
         //   false = Preview is not available.
-        if (isPreview === false) return
+        if (isPreview === false || !previewData) return
 
         // Save the preview data to somewhere globally accessible. This could be
         // something like a global Redux store or React context.
@@ -39,12 +39,11 @@ const PreviewPage = () => {
         // Object.assign(window.__PRISMIC_PREVIEW_DATA__, previewData, {})
         window.__PRISMIC_PREVIEW_DATA__ = previewData
 
-        if (previewData) {
-            if (allPaths.includes(path)) {
-                navigate(path)
-            } else {
-                navigate('/unpublishedPreview')
-            }
+        console.log('preview.js', isPreview, previewData, path)
+        if (allPaths.includes(path)) {
+            navigate(path)
+        } else {
+            navigate('/unpublishedPreview')
         }
     }, [isPreview, previewData, path]);
 
